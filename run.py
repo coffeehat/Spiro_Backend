c549324c42987cda62512ad1598fd34b46e9747f
Hanxiang.Li <hanx.leee@gmail.com>
2023-02-25 16:39:06 +0800

    Add hint to setting token key and email in run.py

--- run.py
+++ run.py
@@ -1,4 +1,15 @@
-from spiro import Server
+from spiro import Server, SpiroConfig
+
+# You must use your own token key, you can set whatever you want, but keep secret
+SpiroConfig.network.token_key = "spiro"
+
+# If you want to use email, you need to set enabled to True and give the parameters below
+SpiroConfig.email.enabled = False
+SpiroConfig.email.send_addr = ""
+SpiroConfig.email.recv_addr = ""
+SpiroConfig.email.port = 25
+SpiroConfig.email.smtp_server = ""
+SpiroConfig.email.password = ""
 
 server = Server()
 server.run()
